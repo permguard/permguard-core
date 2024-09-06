886c25d53b4a2e2eb5c1c40bb59678e3d270aecd
Nicola Gallo <nicolagmt@hotmail.com>
2024-09-06 22:48:02 +0200

    feat: implemented validation to include wildcard names

--- pkg/extensions/validators/validator.go
+++ pkg/extensions/validators/validator.go
@@ -32,18 +32,17 @@ func isSimpleName(fl validator.FieldLevel) bool {
 
 // isName is a custom validator for name.
 func isName(fl validator.FieldLevel) bool {
-	pattern := `^[a-z][a-z0-9\-\._]*[a-z0-9]$`
+	pattern := `^[a-z][a-z0-9\-\._]*[a-z0-9]*$`
 	regex := regexp.MustCompile(pattern)
 	return regex.MatchString(fl.Field().String())
 }
 
 // isWildcardName is a custom validator for wilcard name.
 func isWildcardName(fl validator.FieldLevel) bool {
-	pattern := `^[a-z0-9\*][a-z0-9\-\._\*]*[a-z0-9\*]$`
+	pattern := `^[a-z\*][a-z0-9\*\-\._]*[a-z0-9\*]*$`
 	regex := regexp.MustCompile(pattern)
 	return regex.MatchString(fl.Field().String())
 }
-
 
 // isUUID is a custom validator for UUID.
 func isUUID(fl validator.FieldLevel) bool {
