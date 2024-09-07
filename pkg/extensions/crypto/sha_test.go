a530bba61898566c2dda354f498af08ccd36d0fb
Nicola Gallo <nicolagmt@hotmail.com>
2024-09-07 10:42:02 +0200

    test: working on unit test

--- pkg/extensions/crypto/sha_test.go
+++ pkg/extensions/crypto/sha_test.go
@@ -9,13 +9,13 @@ import (
 func TestComputeSHA256(t *testing.T) {
 	assert := assert.New(t)
 
-	// Test 1: Verifica hash SHA256 di dati vuoti
+	// Test 1: Verify SHA256 hash of empty data
 	data := []byte("")
 	expectedHash := "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 	result := ComputeSHA256(data)
 	assert.Equal(expectedHash, result)
 
-	// Test 2: Verifica hash SHA256 di dati specifici
+	// Test 2: Verify SHA256 hash of specific data
 	data = []byte("Hello, World!")
 	expectedHash = "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f"
 	result = ComputeSHA256(data)
@@ -25,13 +25,13 @@ func TestComputeSHA256(t *testing.T) {
 func TestComputeStringSHA256(t *testing.T) {
 	assert := assert.New(t)
 
-	// Test 1: Verifica hash SHA256 di una stringa vuota
+	// Test 1: Verify SHA256 hash of an empty string
 	data := ""
 	expectedHash := "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 	result := ComputeStringSHA256(data)
 	assert.Equal(expectedHash, result)
 
-	// Test 2: Verifica hash SHA256 di una stringa specifica
+	// Test 2: Verify SHA256 hash of a specific string
 	data = "Hello, World!"
 	expectedHash = "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f"
 	result = ComputeStringSHA256(data)
