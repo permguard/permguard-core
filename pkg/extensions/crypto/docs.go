626a4c2cf6dc35d93c705e2d08fe62e3dda5cdd9
Nicola Gallo <nicolagmt@hotmail.com>
2024-09-07 17:41:20 +0200

    refactor: code refactoring

--- pkg/extensions/crypto/docs.go
+++ pkg/extensions/crypto/docs.go
@@ -13,8 +13,6 @@
 // limitations under the License.
 //
 // SPDX-License-Identifier: Apache-2.0
-//
-// SPDX-License-Identifier: Apache-2.0
 
 // Package crypto implements cryptographic functions.
 package crypto
